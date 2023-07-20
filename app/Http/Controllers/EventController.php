aa75da13c03f1c6f4f9f7c8d57b9045ea08a0d5d
Elena Tonini <e.tonini002@studenti.unibs.it>
2023-07-20 11:09:15 +0200

    minor fix

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -13,7 +13,7 @@ class EventController extends Controller
 {
 
     public function __construct() {
-        $this->middleware('authSecretary', ['only' => ['update', 'create', 'store', 'edit', 'destroy']]);
+        $this->middleware('authSecretary', ['only' => ['show', 'update', 'create', 'store', 'edit', 'destroy']]);
         $this->middleware('authTeacher', ['only' => ['show', 'create', 'store']]);
     }
 
